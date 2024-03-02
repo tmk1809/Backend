03b7fd770a1a648b9a5e25c0f27fa4837427a7d1
Tran Minh Khoi <65523179+tmk1809@users.noreply.github.com>
2024-03-02 10:20:01 +0700

    commit

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+var acl = require('express-acl');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/login');
 
 var app = express();
 
@@ -32,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const { MongoClient } = require("mongodb");
 const UserModel = require("../models/UserModel");
+const data = require("../data");
 var router = express.Router();
 
 router.get('/login', (req, res)=>{
@@ -12,7 +13,29 @@ router.post('/login', async (req, res) => {
     if (!user) return res.status(422).send('Email or Password is not correct');
     const checkPassword = await bcrypt.compare(req.body.password, user.password);
     if (!checkPassword) return res.status(422).send('Email or Password is not correct');
-    return res.send(`User ${user.name} has logged in`);
+    
+    res.redirect('/');
   })
+
+router.get('/register', (req, res)=>{
+    res.render("register");
+})
+
+router.post('/register', async (req, res)=>{
+    var email = req.body.email;
+    var password = req.body.password;
+    UserModel.create({
+        email: email,
+        password: password
+    })
+    .then(data=>{
+        console.log("Register successfully");
+    })
+    .catch(err=>{
+        console.log("Register failed");
+    })
+    console.log("Successful sign up");
+    res.render("login");
+})
 
 module.exports = router;
